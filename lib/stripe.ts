716a41167bcde51be2b5c14355d71d9392a09527
zaid-gd <228304862+zaid-gd@users.noreply.github.com>
2026-03-06 05:43:54 +0530

    feat: Implement Stripe embedded checkout for payment processing.

---
+++ lib/stripe.ts
@@ -0,0 +1,12 @@
+import 'server-only'
+
+import Stripe from 'stripe'
+
+export const stripe = new Stripe(process.env.Roadmap_nexus_STRIPE_SECRET_KEY as string, {
+    // https://github.com/stripe/stripe-node#configuration
+    apiVersion: '2024-12-18.acacia', // Best practice is to set a specific API version
+    appInfo: {
+        name: 'Roadmap Nexus',
+        url: 'https://roadmapnexus.com'
+    }
+})
